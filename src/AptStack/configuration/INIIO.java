2d3578eb76a124bc4e38dd8e6b2a4214db2cc219
Andrew Koerner <andrewkoerner.b@gmail.com>
2011-11-03 21:12:53 -0500

    progress...

--- src/AptStack/configuration/INIIO.java
+++ src/AptStack/configuration/INIIO.java
@@ -1,6 +1,8 @@
 package AptStack.configuration;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import AptStack.exceptions.configuration.*;
 
@@ -19,6 +21,7 @@ public class INIIO{
 				this.setHeader();
 				this.writeHeader();
 			}else{
+				System.out.println(this.file.getAbsolutePath());
 				this.loadFile();
 			}
 		} catch (IOException e) {
@@ -27,7 +30,17 @@ public class INIIO{
 	}
 	
 	private void writeHeader(){
-		
+		try {
+			FileLine pointer = header;
+			BufferedWriter out = new BufferedWriter(new FileWriter(this.file));
+			while(pointer != null){
+				out.write(pointer.line);
+				System.out.println(pointer.line);
+				out.newLine();
+				pointer = pointer.nextLine;
+			}
+			out.close();
+		}catch (IOException e){}
 	}
 	
 	private void loadFile(){
@@ -37,14 +50,30 @@ public class INIIO{
 	private void setHeader(){
 		FileLine pointer = new FileLine("#############################################################################################################################################");
 		this.header = pointer;
+		
 		pointer.nextLine = new FileLine("# Application Configuration                                                                                                                 #");
 		pointer = pointer.nextLine;
-		
-		
-		while(pointer.nextLine != null){
-			System.out.println(pointer.line);
-			pointer = pointer.nextLine;
-		}
+		pointer.nextLine = new FileLine("#                                                                                                                                           #");
+		pointer = pointer.nextLine;
+		pointer.nextLine = new FileLine("# Santax:                                                                                                                                   #");
+		pointer = pointer.nextLine;
+		pointer.nextLine = new FileLine("#    Pound symbol \"#\" acts as a single line comment                                                                                         #");
+		pointer = pointer.nextLine;
+		pointer.nextLine = new FileLine("#    equal sign \"=\" defines an attribute and its associated value                                                                           #");
+		pointer = pointer.nextLine;	
+		pointer.nextLine = new FileLine("#    lines containing no \"=\" and or no no associated value represent Widget headers or names                                                #");
+		pointer = pointer.nextLine;		
+		pointer.nextLine = new FileLine("#    lines containing no \"=\" and or no no associated value and a colon \":\" followed by the name represent an attribute list or collection.  #");
+		pointer = pointer.nextLine;		
+		pointer.nextLine = new FileLine("#    lists are delimited and terminated by whitespace e.g., spaces, tabs, ect.                                                              #");
+		pointer = pointer.nextLine;		
+		pointer.nextLine = new FileLine("#    backslash \"/\" is the escape character if special characters are required in strings                                                    #");
+		pointer = pointer.nextLine;
+		pointer.nextLine = new FileLine("#                                                                                                                                           #");
+		pointer = pointer.nextLine;		
+		pointer.nextLine = new FileLine("#                                                                                                                                           #");
+		pointer = pointer.nextLine;		
+		pointer.nextLine = new FileLine("#############################################################################################################################################");
 	}
 	
 	private class FileLine{
